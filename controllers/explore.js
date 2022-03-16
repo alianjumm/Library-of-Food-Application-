0b152419708a6c966631f787dac7af69e70672c0
Ali Anjum <iianjum@hotmail.com>
2022-03-16 17:01:06 -0400

    styling

--- controllers/explore.js
+++ controllers/explore.js
@@ -2,10 +2,22 @@ const res = require('express/lib/response');
 const Recipe = require('../models/recipe');
 const moment = require('moment')
 
-function index_cuisines_get(req,res) {
+function index_get(req,res) {
     res.render('explore/cuisines')
+}
+
+async function index_cuisines_get(req,res) {
+    let recipes = await Recipe.find({cuisine: req.params.cuisine})
+    res.render(`explore/cuisines/${req.params.cuisine}/main`, {recipes});
+}
+
+async function index_recipe_get(req,res) {
+    let recipe = await Recipe.findById(req.params.id);
+    res.render(`explore/cuisines/${req.params.cuisine}/view`, {recipe})
 }
 
 module.exports = {
     index_cuisines_get,
+    index_get,
+    index_recipe_get
 }
--- routes/explore.js
+++ routes/explore.js
@@ -4,8 +4,9 @@ const isLoggedIn = require("../helper/isLoggedIn")
 
 var exploreCtrl = require('../controllers/explore');
 
-router.get('/recipe/explore', exploreCtrl.index_cuisines_get);
-// router.get('/explore/:cuisine', exploreCtrl.index_get);
+router.get('/recipe/explore', exploreCtrl.index_get);
+router.get('/explore/:cuisine', exploreCtrl.index_cuisines_get);
+router.get('/explore/:cuisine/:id', exploreCtrl.index_recipe_get);
 
 
 module.exports = router;
