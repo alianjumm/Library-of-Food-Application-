64a766b8352273e659fbe84366e15c8e8c88a90f
Ali Anjum <iianjum@hotmail.com>
2022-04-28 19:07:53 -0400

    final

--- routes/explore.js
+++ routes/explore.js
@@ -7,6 +7,7 @@ var exploreCtrl = require('../controllers/explore');
 router.get('/recipe/explore', exploreCtrl.index_get);
 router.get('/explore/:cuisine', exploreCtrl.index_cuisines_get);
 router.get('/explore/:cuisine/:id', exploreCtrl.index_recipe_get);
+router.post('/recipe/view/:id/reviews', recipeCtrl.addReview);
 
 
 module.exports = router;
